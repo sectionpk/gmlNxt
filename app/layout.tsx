7625c4c36e90df8b4738d80ecd0cdcbb49ea00ab
deflk786 <secdefmodlk@gmail.com>
2025-06-09 11:03:30 +0630

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
   title: 'Gmail',
@@ -6,7 +6,11 @@ export const metadata: Metadata = {
   icons: {
     icon: '/images/image.png',
   },
-  viewport: 'width=device-width, initial-scale=1',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
 };
 
 export default function RootLayout({
