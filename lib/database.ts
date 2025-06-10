96a6e6abec86586c34ba3950a529054602809e02
sectionpk <sectionofficer.minfin@gmail.com>
2025-06-10 13:11:29 +0630

    Update database.ts

--- lib/database.ts
+++ lib/database.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db, Collection } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 let dbInstance: Db;
